66f871f4121da2522453760a0bbc9acfdd8bb8d5
Daniel Fabián El Belaizi <daniel.elbelaizi.alu@iescampanillas.com>
2020-10-19 21:45:41 +0200

    Ejercicio 10 casi hecho

---
+++ Ejercicio 6/PiramideAsteriscos.java
@@ -0,0 +1,15 @@
+/**
+ * Muestra por pantalla 10 palabras en inglés con su correspondiente traducción al castellano
+ * 
+ * @autor Daniel Fabián El Belaizi
+ */
+public class PiramideAsteriscos { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("    *");
+    System.out.println("   ***");
+    System.out.println("  *****");
+    System.out.println(" *******");
+    System.out.println("*********");
+  }
+}
+

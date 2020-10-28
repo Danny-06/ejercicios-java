9cbe1ed977fd58eecec734044e327bb5b8558293
Daniel Fabián El Belaizi <daniel.elbelaizi.alu@iescampanillas.com>
2020-10-28 09:13:25 +0100

    nombres actulizados

--- Seccion 4/Ejercicio 01/DiaAsignatura.java
+++ Seccion 4/Ejercicio 01/DiaAsignatura.java
@@ -1,9 +1,9 @@
 /**
- * Muestra por pantalla la frase "¡Hola mundo!"
+ *
  * 
  * @autor Daniel Fabián El Belaizi
  */
-public class MiNombreEnPantalla { // Clase principal
+public class DiaAsignatura { // Clase principal
   public static void main(String[] args) {
     System.out.println("¡Hola mundo!");
   }
